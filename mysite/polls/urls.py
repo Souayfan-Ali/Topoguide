8cdf1644248fcdf932d7a5461641f92a2a1a35ea
John Doe <john.doe@isae-supaero.fr>
2022-04-16 13:44:23 +0200

    creation de 2 page html

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from . import views
-
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.Itineraire_list, name='Itineraire_list'),
+    path('<int:Itineraire_id>/', views.Itineraire_DetailView, name='Itineraire_DetailView'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,20 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+from .models import Itineraire
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def Itineraire_list(request):
+    Itineraire_list = Itineraire.objects.all()
+    template = loader.get_template('polls/itineraire_list.html')
+    context = {
+        'Itineraire_list': Itineraire_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def Itineraire_DetailView(request, Itineraire_id):
+    try:
+        Itineraire_ = get_object_or_404(Itineraire, pk=Itineraire_id)
+    except Itineraire.DoesNotExist:
+        raise Http404("Itineraire does not exist")
+    return render(request, 'polls/Itineraire_Detail.html', {'Itineraire': Itineraire_})
