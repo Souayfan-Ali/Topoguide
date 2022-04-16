6e64edf7cdb635c97b85e3e16ab74bf089d1c221
John Doe <john.doe@isae-supaero.fr>
2022-04-16 23:16:48 +0200

    editer sur les pages et les interagit entre elle'

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,4 +5,5 @@ app_name = 'polls'
 urlpatterns = [
     path('', views.Itineraire_list, name='Itineraire_list'),
     path('<int:Itineraire_id>/', views.Itineraire_DetailView, name='Itineraire_DetailView'),
+    path('<int:Itineraire_id>/Editer_une_Sortie/', views.Editer_une_Sortie, name='Editer_une_Sortie'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.template import loader
 from .models import Itineraire
-
+from .forms import SearchForm 
+from django.http import HttpResponseRedirect
 
 def Itineraire_list(request):
     Itineraire_list = Itineraire.objects.all()
-    template = loader.get_template('polls/itineraire_list.html')
+    template = loader.get_template('polls/Itineraire_list.html')
     context = {
         'Itineraire_list': Itineraire_list,
     }
@@ -17,4 +18,25 @@ def Itineraire_DetailView(request, Itineraire_id):
         Itineraire_ = get_object_or_404(Itineraire, pk=Itineraire_id)
     except Itineraire.DoesNotExist:
         raise Http404("Itineraire does not exist")
-    return render(request, 'polls/Itineraire_Detail.html', {'Itineraire': Itineraire_})
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            Itineraire_ = Itineraire.objects.filter(title__icontains=query)
+            # using HttpResponseRedirect is not appropriate here: albums must be rendered
+        else:
+            Itineraire_ = get_list_or_404(Itineraire)
+    else:
+        form = SearchForm()
+    return render(request, 'polls/Itineraire_Detail.html', {'Itineraire': Itineraire_ , 'form': form})
+
+def Editer_une_Sortie(request, Itineraire_id):
+    try:
+        Itineraire_ = get_object_or_404(Itineraire, pk=Itineraire_id)
+    except Itineraire.DoesNotExist:
+        raise Http404("Itineraire does not exist")
+    template = loader.get_template('polls/Editer_une_Sortie.html')
+    context = {
+        'Itineraire_': Itineraire_,
+    }
+    return HttpResponse(template.render(context, request))
