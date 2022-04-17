8ade36a9e7b6dffb4a3802b878548e9388be4e65
John Doe <john.doe@isae-supaero.fr>
2022-04-18 00:43:26 +0200

    form editing and verified

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.template import loader
-from .models import Itineraire
+from .models import Itineraire,sortie
 from .forms import SearchForm 
 from django.http import HttpResponseRedirect
 
@@ -14,21 +14,28 @@ def Itineraire_list(request):
     return HttpResponse(template.render(context, request))
 
 def Itineraire_DetailView(request, Itineraire_id):
-    try:
-        Itineraire_ = get_object_or_404(Itineraire, pk=Itineraire_id)
-    except Itineraire.DoesNotExist:
-        raise Http404("Itineraire does not exist")
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
-            query = form.cleaned_data['query']
-            Itineraire_ = Itineraire.objects.filter(title__icontains=query)
-            # using HttpResponseRedirect is not appropriate here: albums must be rendered
+            Date = form.cleaned_data['Date']
+            Duree_reelle = form.cleaned_data['Duree_reelle']
+            Nb_participants = form.cleaned_data['Nb_participants']
+            Type_randonneurs = form.cleaned_data['Type_randonneurs']
+            Meteo = form.cleaned_data['Meteo']
+            Itineraire_ = get_object_or_404(Itineraire, pk=Itineraire_id)
+            Difficulte_ressentie = form.cleaned_data['Difficulte_ressentie']
+            ins = sortie(itineraire=Itineraire_,date_de_sortie=Date,duree_reelle=Duree_reelle,nombre_de_personne=Nb_participants,experience=Type_randonneurs,meteo=Meteo,difficulte=Difficulte_ressentie)
+            ins.save()
         else:
             Itineraire_ = get_list_or_404(Itineraire)
     else:
         form = SearchForm()
-    return render(request, 'polls/Itineraire_Detail.html', {'Itineraire': Itineraire_ , 'form': form})
+    try:
+        Itineraire_ = get_object_or_404(Itineraire, pk=Itineraire_id)
+    except Itineraire.DoesNotExist:
+        raise Http404("Itineraire does not exist")
+    sortie_list = sortie.objects.filter(itineraire=Itineraire_)
+    return render(request, 'polls/Itineraire_Detail.html', {'Itineraire': Itineraire_ , 'form': form, 'sortie_list': sortie_list})
 
 def Editer_une_Sortie(request, Itineraire_id):
     try:
